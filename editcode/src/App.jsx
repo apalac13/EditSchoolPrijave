8fb48e3b932ae4c267f8dfd23c603e76987e002f
Ana <apalac13>
2024-05-02 19:31:51 +0200

    predavac deleted

--- editcode/src/App.jsx
+++ editcode/src/App.jsx
@@ -6,7 +6,6 @@ import Radionice from "./pages/Radionice";
 import Predavaci from "./pages/Predavaci";
 import Administracija from "./pages/Administracija";
 import Navigacija from "./components/Navigacija";
-import Predavac from "./components/predavaciComponents/Predavac";
 
 function App() {
   const [korisnik, postaviKorisnika] = useState(false);
@@ -21,7 +20,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Radionice />} />
           <Route path="/predavaci" element={<Predavaci />} />
-          <Route path="/predavaci/:id" element={<Predavac />} />
           <Route path="/administracija" element={<Administracija />} />
         </Routes>
       </userContext.Provider>
