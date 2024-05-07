cf15db8b5a407df6b62fa5821429539703785cb3
Ana <apalac13>
2024-05-07 15:13:25 +0200

    validation modal form done

--- editcode/src/components/radioniceComponents/radionicaComponents/Modal.jsx
+++ editcode/src/components/radioniceComponents/radionicaComponents/Modal.jsx
@@ -1,38 +1,67 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function Modal({ radionica, setRadionice, modal, setModal }) {
   const [success, setSuccess] = useState(false);
+  const [novaPrijava, setNovaPrijava] = useState({
+    ime: "",
+    email: "",
+    razlog: "",
+  });
 
-  // const emailValidacija = email.includes("@");
-  // const osobaValidacija = osoba.length >= 3;
-  // const razlogValidacija = adresa.length >= 5;
+  const [prijave, setPrijave] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3003/prijave")
+      .then((rez) => setPrijave(rez.data))
+      .catch((error) => console.log("Error:", error.message));
+  }, []);
+
+  const emailValidacija = novaPrijava.email.includes("@");
+  const imeValidacija = novaPrijava.ime.length >= 3;
+  const razlogValidacija = novaPrijava.razlog.length >= 5;
 
   const prijavaRadionice = async () => {
     try {
-      const updatedRadionica = {
-        broj_prijava: radionica.broj_prijava + 1,
-      };
+      if (emailValidacija && imeValidacija && razlogValidacija) {
+        const updatedRadionica = {
+          broj_prijava: radionica.broj_prijava + 1,
+        };
 
-      await axios
-        .patch(
-          `http://localhost:3003/radionice/${radionica.id}`,
-          updatedRadionica
-        )
-        .then((rez) => {
-          setRadionice((stanje) => {
-            return stanje.map((item) =>
-              item.id === radionica.id
-                ? { ...item, broj_prijava: updatedRadionica.broj_prijava }
-                : item
-            );
+        await axios
+          .patch(
+            `http://localhost:3003/radionice/${radionica.id}`,
+            updatedRadionica
+          )
+          .then((rez) => {
+            setRadionice((stanje) => {
+              return stanje.map((item) =>
+                item.id === radionica.id
+                  ? { ...item, broj_prijava: updatedRadionica.broj_prijava }
+                  : item
+              );
+            });
           });
-        });
-      setSuccess(true);
+        await axios
+          .post("http://localhost:3003/prijave", novaPrijava)
+          .then((rez) => {
+            setPrijave((stanje) => [...stanje, rez.data]);
+          })
+          .catch((error) => console.log(error.message));
+        setSuccess(true);
+      } else {
+        alert("Pokusajte ponovo");
+      }
     } catch (error) {
       console.log("Error: ", error.message);
     }
+  };
+
+  const promjenaPodatka = (event) => {
+    const { name, value } = event.target;
+    setNovaPrijava({ ...novaPrijava, [name]: value });
   };
 
   return (
@@ -57,27 +86,52 @@ function Modal({ radionica, setRadionice, modal, setModal }) {
             }}
             className="flex flex-col gap-4 px-14 "
           >
-            <input
-              type="text"
-              name="ime"
-              placeholder="puno ime"
-              className="w-full h-[40px] border border-blue-47 rounded-md p-1"
-              required
-            />
+            <div className="flex flex-col">
+              <input
+                type="text"
+                name="ime"
+                value={novaPrijava.ime}
+                onChange={promjenaPodatka}
+                placeholder="puno ime"
+                className="w-full h-[40px] border border-blue-47 rounded-md p-1"
+                required
+              />
+              {!imeValidacija && novaPrijava.ime.length > 0 && (
+                <p className="validacija">
+                  Ime mora sadržavati najmanje 3 znaka
+                </p>
+              )}
+            </div>
+            <div className="flex flex-col">
+              <input
+                type="email"
+                name="email"
+                value={novaPrijava.email}
+                onChange={promjenaPodatka}
+                placeholder="email"
+                className="w-full h-[40px] border border-blue-47 rounded-md p-1"
+                required
+              />
+              {!emailValidacija && novaPrijava.email.length > 0 && (
+                <p className="validacija">Email mora sadržavati znak "@"</p>
+              )}
+            </div>
+            <div className="flex flex-col">
+              <textarea
+                name="razlog"
+                value={novaPrijava.razlog}
+                onChange={promjenaPodatka}
+                placeholder="razlog prijave"
+                className="w-full h-[80px] border border-blue-47 rounded-md p-1"
+                required
+              ></textarea>
+              {!razlogValidacija && novaPrijava.razlog.length > 0 && (
+                <p className="validacija">
+                  Ime mora sadržavati najmanje 3 znaka
+                </p>
+              )}
+            </div>
 
-            <input
-              type="email"
-              name="email"
-              placeholder="email"
-              className="w-full h-[40px] border border-blue-47 rounded-md p-1"
-              required
-            />
-            <textarea
-              name="opis"
-              placeholder="razlog prijave"
-              className="w-full h-[80px] border border-blue-47 rounded-md p-1"
-              required
-            ></textarea>
             <button
               type="submit"
               className="mt-3 border border-blue-46 bg-blue-46 hover:bg-blue-46/80 text-white-70 w-full h-[35px] rounded-md "
